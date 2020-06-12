9512985ef6909894e3cbf460230b85d47843ff43
Lorenzo Bianconi <lorenzo@kernel.org>
2020-06-12 19:23:55 +0200

    mt76: mt7615: wake device before pulling packets from mac80211 queues
    Make sure the device is in full-power before pulling frames from
    mac80211 queues
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- mt7615/mac.c
+++ mt7615/mac.c
@@ -1799,9 +1799,13 @@ void mt7615_pm_wake_work(struct work_struct *work)
 						pm.wake_work);
 	mphy = dev->phy.mt76;
 
-	if (mt7615_driver_own(dev))
+	if (mt7615_driver_own(dev)) {
 		dev_err(mphy->dev->dev, "failed to wake device\n");
+		goto out;
+	}
 
+	tasklet_schedule(&dev->mt76.tx_tasklet);
+out:
 	clear_bit(MT76_STATE_WAKE_SCHED, &mphy->state);
 	complete_all(&dev->pm.wake_cmpl);
 }
